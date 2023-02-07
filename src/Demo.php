deeb4cb264bbacd82f199a7c910f80c16bd21d13
xushao <543871204@qq.com>
2023-02-07 16:46:44 +0800

    q

--- src/Demo.php
+++ src/Demo.php
@@ -4,5 +4,13 @@ namespace textComposer;
 
 class Demo
 {
+    public function index()
+    {
+        return('Demo---ff');
+    }
 
+    public function demo1()
+    {
+        return('Demo----44');
+    }
 }
